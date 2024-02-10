9d20de06332b537f0795d4f6cbe55d7cc126d231
Rook <>
2024-02-10 17:54:43 +1100

    Added news functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord import app_commands
 import sys
 import random
 import time
+from news import preamble, cata, catb, catc, closing, corps, gangs, areas, plans, accidents, mysteries
 
 #Night market categories
 categories = ["Food and drugs", "Personal Electronics", "Weapons and Armor", "Cyberware", "Clothing and Fashionware", "Survival Gear"]
@@ -79,6 +80,58 @@ async def nightmarket(interaction: discord.Interaction):
         inv = tuple(random.sample(goods, count))
         """Lists the goods"""
         await interaction.followup.send(f'**> We have {count} offerings of {n}:**{new_line}> * {"\n > * ".join(str(x) for x in inv)}')
+
+@client.tree.command()
+async def news(interaction: discord.Interaction):
+
+    intro = random.choice(preamble)
+    topica = random.choice(cata)
+    topicb = random.choice(catb)
+    topicc = random.choice(catc)
+    outro  = random.choice(closing)
+
+    if topica == "An undisclosed corp":
+        newsa = random.choice(corps)
+    elif topica == "a gang":
+        newsa = random.choice(gangs)
+    elif topica == "a gang of rich investors":
+        newsa = ("a gang of rich investors from " + random.choice(corps))
+    elif topica == "some anonymous Fixer":
+        newsa = ("some anonymous Fixer based in " + random.choice(areas))
+    elif topica == "an alliance of corpos":
+        corpgang = random.sample(corps, 2)
+        tidiedcorpgang = (corpgang[0] + " and " + corpgang[1])
+        newsa = ("an alliance of corpos from " + tidiedcorpgang)
+    else:
+        newsa = topica
+
+    if topicb == "is threatening":
+        newsb = ("is threatening " + random.choice(cata) + " with")
+    else:
+        newsb = topicb
+
+    if topicc == "a plan.":
+        newsc = ("a plan to " + random.choice(plans))
+    elif topicc == "a compromise.":
+        newsc = ("a compromise by agreeing to " + random.choice(plans))
+    elif topicc == "an accident.":
+        newsc = ("an accident " + random.choice(accidents))
+    elif topicc == "a mysterious man.":
+        newsc = ("a mysterious man, all we know about him is he " + random.choice(mysteries))
+    elif topicc == "a mysterious woman.":
+        newsc = ("a mysterious woman, all we know about her is she " + random.choice(mysteries))
+    else:
+        newsc = topicc
+
+    print("The intro is", intro)
+    print("The first category is", topica)
+    print("The Second category is", topicb)
+    print("The Third category is", topicc)
+    print("The Outro is", outro)
+
+    print("The News is")
+    print(intro, newsa, newsb, newsc, outro)
+    await interaction.response.send_message(f'> ## {intro} {newsa} {newsb} {newsc} {outro}')
 
 # To make an argument optional, you can either give it a supported default argument
 # or you can mark it as Optional from the typing standard library. This example does both.
